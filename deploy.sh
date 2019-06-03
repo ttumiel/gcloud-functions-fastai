493db30945ae430ebeb502b41c74d19d7d4c6338
Thomas Tumiel <ttumiel@gmail.com>
2019-06-03 11:22:50 +0200

    add warning about init

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
+echo "Ensure you have run `gcloud init` before running this"
+echo "-----------------------------------------------------"
 echo "Deploying text endpoint function"
 gcloud functions deploy text_endpoint --runtime python37 --trigger-http
