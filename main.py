0ff1e1443b42a9b3896e9275384c4c9cd11af07f
Thomas Tumiel <ttumiel@gmail.com>
2019-06-08 17:44:17 +0200

    change app endpoint to a decorator

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 from flask import Flask, escape, jsonify#, request
-from utils import generate_text
+from utils import generate_text, classify_text
+import random
+
+from config import DEFAULT_WORDS
 # app = Flask(__name__)
 
-# @app.route('/', methods=['GET', 'POST'])
-def text_endpoint(request):
-    """HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-    Returns:
-        The response text.
-    """
-    request_json = request.get_json(silent=True)
-    request_args = request.args
+def endpoint(func):
+    def wrapper(request):
+        request_json = request.get_json(silent=True)
+        request_args = request.args
 
-    if request_json and 'data' in request_json:
-        data = request_json['data']
-    elif request_args and 'data' in request_args:
-        data = request_args['data']
-        # Get optional parameters here
-    else:
-        return "Error"
-    return jsonify({'text': generate_text(data)})
+        if request_json and 'data' in request_json:
+            data = request_json['data']
+        elif request_args and 'data' in request_args:
+            data = request_args['data']
+            # Get optional parameters here
+        else:
+            data = random.choice(DEFAULT_WORDS)
+        
+        resp = func(data)
+        return jsonify(resp)
+    return wrapper
+
+# @app.route('/', methods=['GET', 'POST'])
+@endpoint
+def text_gen(data):
+    "Text generation cloud function."
+    return {'text': generate_text(data)}
+
+@endpoint
+def text_clas(data):
+    return classify_text(data)
