f4ee27f9dcd088d4885707d469022cbb6b214105
Thomas Tumiel <ttumiel@gmail.com>
2019-06-11 19:31:16 +0200

    rearrange

--- test.py
+++ test.py
@@ -1,17 +1,16 @@
-"Helps testing functions that you want to deploy."
+"Helps test functions that you want to deploy."
 from flask import Flask
-app = Flask(__name__)
-
 
 class DummyRequest():
     def __init__(self, json_data, query_str_data):
         self.json_data = json_data
         self.args = query_str_data
 
-    def get_json(self, **kwargs):
+    def get_json(self, *args, **kwargs):
         return self.json_data
 
 def test_function(func, json_data=None, query_str_data=None):
+    app = Flask(__name__)
     req = DummyRequest(json_data, query_str_data)
     with app.app_context():
         return func(req).get_json()
